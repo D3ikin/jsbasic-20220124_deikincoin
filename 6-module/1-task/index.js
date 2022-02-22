dd1d3513bc19b7cae10a52a9decab862fa3918a7
D3ikin <deikincoin@gmail.com>
2022-02-22 12:02:49 +0200

    решение задачи 1, модуль 6

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,18 +1,56 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
 export default class UserTable {
   constructor(rows) {
+    this.data = rows;
+    this.elem = this.render();
   }
+
+  render() {
+    this.tableEl = document.createElement('TABLE');
+    this.tHead = document.createElement('THEAD');
+    this.tBody = document.createElement('TBODY');
+
+    this.tHead.insertAdjacentHTML('beforeEnd',
+    `
+      <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+      </tr>
+    `)
+
+    this.tableEl.append(this.tHead);
+    this.tableEl.append(this.tBody);
+
+
+    for (const userInfo of this.data) {
+      this.tBody.insertAdjacentHTML('beforeEnd',
+        `
+          <tr>
+            <td>${userInfo.name}</td>
+            <td>${userInfo.age}</td>
+            <td>${userInfo.salary}</td>
+            <td>${userInfo.city}</td>
+            <td><button>X</button></td>
+          </tr>
+        `)
+		}
+
+    this.tableEl.addEventListener('click', this.onClick)
+
+    return this.tableEl
+  }
+
+  onClick(event) {
+    let clickBtn = event.target.tagName === 'BUTTON';
+
+    if(clickBtn) {
+      event.target.closest('tr').remove();
+    }
+  }
+
 }
+
+
+
